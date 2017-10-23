231465f422eedfda7a2b5fef4f113b4434ec2615
Kenia Valladarez <keniavalladarez@gmail.com>
2017-10-22 21:21:59 -0400

    Using nunjucks templating to display the movies from mongodb.

--- app.js
+++ app.js
@@ -1,42 +1,30 @@
 var express = require('express'),
-    app = express();
-
-app.get('/', function(req, res){
-    res.send('Hello World');
-});
-
-app.use(function(req, res){
-    res.sendStatus(404);
-});
-
-var server = app.listen(3000, function() {
-    var port = server.address().port;
-    console.log('Express server listening on port %s', port);
-});
-
-
-var MongoClient = require('mongodb').MongoClient,
+    app = express(),
+    engines = require('consolidate'),
+    MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
-var url = 'mongodb://localhost:27017/video';
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect('mongodb://localhost:27017/video', function(err, db) {
 
     assert.equal(null, err);
-    console.log("Successfully connected to server");
+    console.log("Successfully connected to MongoDB.");
 
-    // Find some documents in our collection
-    db.collection('movies').find({}).toArray(function(err, docs) {
-
-        // Print the documents returned
-        docs.forEach(function(doc) {
-            console.log(doc.title);
+    app.get('/', function(req, res){
+        db.collection('movies').find({}).toArray(function(err, docs) {
+            res.render('movies', { 'movies': docs } );
         });
-
-        // Close the DB
-        db.close();
     });
 
-    // Declare success
-    console.log("Called find()");
+    app.use(function(req, res){
+        res.sendStatus(404);
+    });
+
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log('Express server listening on port %s.', port);
+    });
 });
